d94a636d4c41349419b53b3944aea6cf2ae55c32
alex9999-afk <yiklungl@gmail.com>
2022-11-28 12:25:40 -0800

    Change1

--- YoutubeVideoDownload/hello.py
+++ YoutubeVideoDownload/hello.py
@@ -1,1 +1,13 @@
-print("hello")
+q_list = ["蘋果的英文是？", "10-8 = ?"]
+a_list = ["apple", "2"]
+
+score = 0
+
+for q,a in zip(q_list,a_list):
+    print(q, end=' ')
+    ans = input().lower().strip()
+
+    if ans == a:
+        print("Great Job!")
+    else:
+            print("Never Give Up!")
